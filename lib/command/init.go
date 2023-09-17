a59b39d3ffe2d48edbbcb5937b49436b94a53f09
i78s <inaba182@gmail.com>
2023-09-17 22:03:02 +0900

    15.4.1. The master branch

--- lib/command/init.go
+++ lib/command/init.go
@@ -1,11 +1,14 @@
 package command
 
 import (
+	"building-git/lib/repository"
 	"fmt"
 	"io"
 	"os"
 	"path/filepath"
 )
+
+var DEFAULT_BRANCH = "master"
 
 func Init(args []string, stdout, stderr io.Writer) int {
 	var path string
@@ -27,7 +30,7 @@ func Init(args []string, stdout, stderr io.Writer) int {
 	}
 
 	gitPath := filepath.Join(rootPath, ".git")
-	dirs := []string{"objects", "refs"}
+	dirs := []string{"objects", "refs/heads"}
 
 	for _, dir := range dirs {
 		err := os.MkdirAll(filepath.Join(gitPath, dir), os.ModePerm)
@@ -36,6 +39,11 @@ func Init(args []string, stdout, stderr io.Writer) int {
 			return 1
 		}
 	}
+
+	refs := repository.NewRefs(gitPath)
+	headPath := filepath.Join("refs", "heads", DEFAULT_BRANCH)
+	refs.UpdateHead(fmt.Sprintf("ref: %s", headPath))
+
 	fmt.Fprintf(stdout, "Initialized empty Jit repository in %s\n", gitPath)
 	return 0
 }
